3d55a29b8e443d074cdfbe5c1458c38a0fb6e45f
John <john'semail>
2022-11-23 20:06:44 -0500

    returning 5 cats and clues ok

--- jeopardy/jeopardy.js
+++ jeopardy/jeopardy.js
@@ -1,21 +1,71 @@
-function ranArray(){
-    let rArray = []
-    for(i=0;i<5;i++){
-        let gen = Math.round(Math.random()*5000)
-        rArray.push(gen)
-    }
-    console.log(rArray)
-    return rArray
+class Clue{
+    constructor(question,answer,showing = "null"){
+        this.question = question;
+        this.answer = answer;
+        this.showing = showing;
+        }
 }
 
-async function pullJeop(num){
-    let response = await axios.get("https://jservice.io/api/categories?count="+num)
-    console.log(response)
+//next: get randFive to spit out a Game object with 5 categories, 5 Clues each
+//then we map those categories onto the DOM
+
+
+//generate a random array of 5 numbers
+function ranGen(max){
+    let rArray = []
+    if(max <= 5) {return [0,1,2,3,4]}
+    else{
+    for(let i=0;i<5;i++){
+        let gen = Math.floor(Math.random()*max)
+        rArray.push(gen)
+    }
+    return rArray
+}
+}
+
+//generate a random UNIQUE arraw of 5 numbers (fails at 100 tries)
+function ranArray(max){
+    for (let i = 0; i <10; i++){
+        let rArray = ranGen(max)
+        if (rArray.length === new Set(rArray).size) //ensures that everything generated was unique
+        {return rArray}
+    }
+    
 }
 
 async function catClues(id){
-    let response = await axios.get("https://jservice.io/api/category?id="+id)
+    let response = await axios.get("https://jservice.io/api/category?id="+id) //returning 1 category object
+    let allClues = response.data.clues //store all the clues in an array
     console.log(response)
+    // generate 5 random clues
+    let cluesTotal = response.data.clues.length //store how many clues there are
+    let clueVals = ranArray(cluesTotal) //array of 5 random numbers within the length of how many clues there are
+    console.log(cluesTotal)
+    console.log(clueVals)
+    let jCat = {}
+    jCat.catname = response.data.title
+    for(let i=0;i<5;i++){
+        let cIndex = clueVals[i]
+        let tClue = allClues[cIndex]
+        console.log(tClue)
+        let uClue = new Clue(tClue.question,tClue.answer)
+        let qname = "q"+i
+        jCat[qname] = uClue
+    }
+    console.log(jCat) //return 5 category names and 5 clues from each category, sort by value
+    return jCat
+}
+
+//randfive returns an array of 5 random categories and 5 of their clues
+async function randFive(){
+    let categories = []
+    let nums = ranArray(5000);
+    for (num of nums){
+        categories.push(await catClues(num))
+    }
+    console.log("here's our categories")
+    console.log(categories)
+    return categories
 }
 
 // categories is the main data structure for the app; it looks like this:
